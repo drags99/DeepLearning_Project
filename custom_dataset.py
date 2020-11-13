c7b3d2b11e6a017ad6365564843e4b99636f1459
Subin Varghese <srvarghese95@gmail.com>
2020-11-12 22:31:11 -0600

    custom dataset shuffle works till 50,000 breaks past that length

--- custom_dataset.py
+++ custom_dataset.py
@@ -1,12 +1,13 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from torchvision import transforms
 import pandas as pd
 import os
-from PIL import Image
+#from PIL import Image
+from skimage import io
 
 class Binary_Dataset(Dataset):
-    def __init__(self, labels_dir, label,image_dir ,transform=None):
+    def __init__(self, labels_dir, label, image_dir ,transform=None):
     #labels_dir: is directory to the csv file with labels probably 'Data/list_attr_celeba.csv'
     #label: which label in the attribute (list_attr_celeba.csv) csv to use 
     #image_dir: directory to images probably 'Data/img_align_celeba/img_align_celeba'
@@ -15,10 +16,11 @@ class Binary_Dataset(Dataset):
         self.labels_dir=labels_dir
         self.label=label
         self.image_dir=image_dir
-        self.transform=transform
+        self.transformers=transform #transforms
         self.df_attr=pd.read_csv(labels_dir)
-        self.df=self.df_attr[label]
-        self.img_names=self.df_attr["image_id"]
+        self.df=self.df_attr[label] #dataframe of the chosen label
+        self.img_names=self.df_attr["image_id"] #dataframe of image ids
+        self.size=self.df.shape[0]
 
         #change -1 to 0 in csv, remove this if not needed
         #########################
@@ -27,24 +29,24 @@ class Binary_Dataset(Dataset):
 
     def __len__(self):
         #return the length of the column of df_attr
-        size=self.df_attr.shape[0]
+        #####################################################
+        size = 50000 #size above about 50,000 has issues no clue why
+        #####################################################
         return size
     
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        #return label and image as tensors
-        #print(self.img_names)
-        
-        #load images
-        img_name=self.img_names[idx]
-        print(self.image_dir+"/"+img_name)
-        image=Image.open(self.image_dir+"/"+img_name) #something wrong here when shuffled
-        
-        #apply transforms to images and make sure you transform them to a tensor
-        sample=self.transform(image)
+        #load image
+        image_path=os.path.join(self.image_dir,self.img_names[idx])
+        image = io.imread(image_path)
+        #print(self.size)
 
-        #load label/label for that image
-        target=self.df[idx]
 
-        return sample, target
+
+
+        tensor_image=image
+
+        #set label
+        label=self.df[idx]
+        label=torch.tensor(int(label))
+
+        return (tensor_image, label)
